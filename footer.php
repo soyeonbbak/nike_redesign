dd10a8b6f4328bbb3ec2460dfe838a8ff2102d2c
soyeonbbak <137027250+soyeonbbak@users.noreply.github.com>
2023-08-25 18:04:55 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,50 @@
+<footer><!-- ok -->
+        <div class="f_left" id="f_left1">
+            <a href="#">새로운 소식</a>
+            <a href="#">멤버가입</a>
+            <a href="#">매장안내</a>
+            <a href="#">나이키 저널</a>
+        </div>
+        <div class="f_left"  id="f_left2">
+            <a href="#">도움말</a>
+            <a href="#" class="f_menu_sub">로그인 안내</a>
+            <a href="#" class="f_menu_sub">주문배송조회</a>
+            <a href="#" class="f_menu_sub">반품 정책</a>
+            <a href="#" class="f_menu_sub">결제 방법</a>
+            <a href="#" class="f_menu_sub">공지사항</a>
+            <a href="#" class="f_menu_sub">문의하기</a>
+            <a href="#">ABOUT NIKE</a>
+            <a href="#" class="f_menu_sub">소식</a>
+            <a href="#" class="f_menu_sub">채용</a>
+            <a href="#" class="f_menu_sub">투자자</a>
+            <a href="#" class="f_menu_sub">지속가능성</a>
+        </div>
+        <div class="f_left"  id="f_left3">
+            <a href="#">이용약관</a>
+            <a href="#">개인정보처리방침</a>
+            <a href="#">위치 기반 서비스 약관</a>
+            <a href="#">영상정보처리기기 운영방침</a>
+        </div>
+        <div class="f_left"  id="f_left4">
+            <p><em>나이키코리아</em></p>
+            <address>
+                주소 :  서울 강남구 테헤란로 152 강남파이낸스센터 30층 <br>
+                (유)나이키코리아 대표 : Kimberlee Lynn Chang Mendes, 킴벌리 린 창 멘데스 <br>
+                통신판매신고번호 : 통신판매업신고번호 2011-서울강남-03461 <br>
+                사업자등록번호 : 등록번호 220-88-09068 <a href="#">사업자 정보 확인</a><br>
+                호스팅 서비스 사업자 : (유)나이키코리아
+            </address>
+            <p><em>고객센터</em></p>
+            <div id="f_info">
+                <p>전화 문의 : 080-022-0182</p>
+                <p>FAX : 02-6744-5880</p>
+                <p>이메일 : service@nike.co.kr</p>
+            </div>
+        </div>
+        <div id="f_right_logo">
+            <a href="#"><img src="./image/sns_twitter.png" alt="트위터"></a>
+            <a href="#"><img src="./image/sns_facebook.png" alt="페이스북"></a>
+            <a href="#"><img src="./image/sns_insta.png" alt="인스타그램"></a>
+            <a href="#"><img src="./image/sns_youtube.png" alt="유튜브"></a>
+        </div>
+    </footer>
